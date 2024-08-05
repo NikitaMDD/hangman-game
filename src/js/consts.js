6fc07943dc745b640f5bbe2a0d714a9f3bad43e5
NikitaMDD <nikitamarch702@gmail.com>
2024-08-05 15:26:37 +0300

    Add more words

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,2 +1,15 @@
-export const WORDS = ['code', 'frontend', 'programming'];
-export const KEYBOARDS_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+export const WORDS = [
+  "code",
+  "frontend",
+  "programming",
+  "abandon",
+  "blanket",
+  "cousin",
+  "defeat",
+  'engineer',
+  'fashion',
+  'frequency',
+  'gold',
+  'intelligence',
+];
+export const KEYBOARDS_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
